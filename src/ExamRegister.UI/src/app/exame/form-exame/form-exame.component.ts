36bf9e03a8805d4e70686bf3464757b84d14a326
Gabriel L. Pontani <gleonelpontani@gmail.com>
2021-06-02 17:43:34 -0300

    tela exame responsiva

--- src/ExamRegister.UI/src/app/exame/form-exame/form-exame.component.ts
+++ src/ExamRegister.UI/src/app/exame/form-exame/form-exame.component.ts
@@ -25,18 +25,16 @@ import { delay } from 'rxjs/operators';
 @Component({
   selector: 'app-form-exame',
   templateUrl: './form-exame.component.html',
-  styleUrls: ['./form-exame.component.scss']
+  styleUrls: ['./form-exame.component.scss'],
 })
 export class FormExameComponent extends BaseFormComponent implements OnInit {
-  
-  imagemForm!: FormGroup;
-
+  editarImagem: boolean = false;
   exame!: ExameDTO;
-  descImagem: string = "";
+  descImagem: string = '';
   modalRef!: BsModalRef;
 
   get paciente(): PacienteDTO {
-    return Object.assign({}, this.formulario.value).paciente
+    return Object.assign({}, this.formulario.value).paciente;
   }
 
   get orgao(): OrgaoDTO {
@@ -92,97 +90,108 @@ export class FormExameComponent extends BaseFormComponent implements OnInit {
     private location: Location,
     private route: ActivatedRoute,
     private datepipe: DatePipe,
-    private router: Router,
+    private router: Router
   ) {
     super();
   }
 
   ngOnInit(): void {
-    this.formulario = this.formBuilder.group({
-      idexame: [null],
-      idexterno: [null, [Validators.required, Validators.maxLength(20)]],
-      datain: [null, Validators.required],
-      dataout: [null, Validators.required],
-      idpaciente: [null, Validators.required],
-      idorgao: [null, Validators.required],
-      idpeca: [null, Validators.required],
-      idmedicoresp: [null, Validators.required],
-      idmedicosolic: [null, Validators.required],
-      idreuniao: [null, Validators.required],
-      idclinica: [null, Validators.required],
-      diagnostico: [null, [Validators.required, Validators.maxLength(10000)]],
-      dadosclinicos: [null, [Validators.required, Validators.maxLength(300)]],
-      suspeitaclinica: [null, [Validators.required, Validators.maxLength(300)]],
-      peso: [null, Validators.required],
-      altura: [null, Validators.required],
-      ast: [null, Validators.required],
-      alt: [null, Validators.required],
-      ggt: [null, Validators.required],
-      colesterol: [null, Validators.required],
-      gamaglobulina: [null, Validators.required],
-      triglicerides: [null, Validators.required],
-      glicemia: [null, Validators.required],
-      falc: [null, Validators.required],
-      biltotal: [null, Validators.required],
-      bildireta: [null, Validators.required],
-      outros: [null, [Validators.maxLength(300)]],
-      inativo: [null],
-      idgrupomedico: [null, Validators.required],
-      conclusao: [null, [Validators.required, Validators.maxLength(300)]],
-      observacao: [null],
-      transplantadofigado: [null],
-      hipertensaoarterial: [null],
-      ictericia: [null],
-      ascite: [null],
-      edema: [null],
-      circulacaocolateral: [null],
-      varizesofagicas: [null],
-      hepatomegalia: [null],
-      figadoendurecido: [null],
-      figadonodular: [null],
-      esplenomegalia: [null],
-      datatransplante: [null],
-      descdoencaautoimune: [null],
-      doencaautoimune: [null],
-      usamedicamento: [null],
-      descusamedicamento: [null],
-      autoanticorpos: [null],
-      vhc: [null],
-      descvhc: [null],
-      vhb: [null],
-      descvhb: [null],
-      hiv: [null],
-      deschiv: [null],
+    this.formulario = this.formBuilder.group(
+      {
+        idexame: [null],
+        idexterno: [null, [Validators.required, Validators.maxLength(20)]],
+        datain: [null, Validators.required],
+        dataout: [null, Validators.required],
+        idpaciente: [null, Validators.required],
+        idorgao: [null, Validators.required],
+        idpeca: [null, Validators.required],
+        idmedicoresp: [null, Validators.required],
+        idmedicosolic: [null, Validators.required],
+        idreuniao: [null, Validators.required],
+        idclinica: [null, Validators.required],
+        diagnostico: [null, [Validators.required, Validators.maxLength(10000)]],
+        dadosclinicos: [null, [Validators.required, Validators.maxLength(300)]],
+        suspeitaclinica: [
+          null,
+          [Validators.required, Validators.maxLength(300)],
+        ],
+        peso: [null, Validators.required],
+        altura: [null, Validators.required],
+        ast: [null, Validators.required],
+        alt: [null, Validators.required],
+        ggt: [null, Validators.required],
+        colesterol: [null, Validators.required],
+        gamaglobulina: [null, Validators.required],
+        triglicerides: [null, Validators.required],
+        glicemia: [null, Validators.required],
+        falc: [null, Validators.required],
+        biltotal: [null, Validators.required],
+        bildireta: [null, Validators.required],
+        outros: [null, [Validators.maxLength(300)]],
+        inativo: [null],
+        idgrupomedico: [null, Validators.required],
+        conclusao: [null, [Validators.required, Validators.maxLength(300)]],
+        observacao: [null],
+        transplantadofigado: [null],
+        hipertensaoarterial: [null],
+        ictericia: [null],
+        ascite: [null],
+        edema: [null],
+        circulacaocolateral: [null],
+        varizesofagicas: [null],
+        hepatomegalia: [null],
+        figadoendurecido: [null],
+        figadonodular: [null],
+        esplenomegalia: [null],
+        datatransplante: [null],
+        descdoencaautoimune: [null],
+        doencaautoimune: [null],
+        usamedicamento: [null],
+        descusamedicamento: [null],
+        autoanticorpos: [null],
+        vhc: [null],
+        descvhc: [null],
+        vhb: [null],
+        descvhb: [null],
+        hiv: [null],
+        deschiv: [null],
 
-      paciente: [null, Validators.required],
-      orgao: [null, Validators.required],
-      peca: [null, Validators.required],
-      grupodeMedico: [null, Validators.required],
-      medicoSolic: [null, Validators.required],
-      clinica: [null, Validators.required],
-      medicoResp: [null, Validators.required],
-      reuniao: [null, Validators.required],
-      examemedicorespdiagnostico: [null, Validators.required],
-      categoriaexame: [null, Validators.required],
-      imagem: [null],
-    }, { updateOn: '' });
+        paciente: [null, Validators.required],
+        orgao: [null, Validators.required],
+        peca: [null, Validators.required],
+        grupodeMedico: [null, Validators.required],
+        medicoSolic: [null, Validators.required],
+        clinica: [null, Validators.required],
+        medicoResp: [null, Validators.required],
+        reuniao: [null, Validators.required],
+        examemedicorespdiagnostico: [null, Validators.required],
+        categoriaexame: [null, Validators.required],
+        imagem: [null],
+      },
+      { updateOn: '' }
+    );
 
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       let idexame = params['id'];
 
       if (idexame) {
-        this.exameService.loadByID(idexame).subscribe(exame => {
-
+        this.exameService.loadByID(idexame).subscribe((exame) => {
           var _exame = Object.assign({}, this.formulario.value);
 
           _exame = exame;
 
-          _exame.transplantadofigado = this.convertString(exame.transplantadofigado);
-          _exame.hipertensaoarterial = this.convertString(exame.hipertensaoarterial);
+          _exame.transplantadofigado = this.convertString(
+            exame.transplantadofigado
+          );
+          _exame.hipertensaoarterial = this.convertString(
+            exame.hipertensaoarterial
+          );
           _exame.ictericia = this.convertString(exame.ictericia);
           _exame.edema = this.convertString(exame.edema);
           _exame.ascite = this.convertString(exame.ascite);
-          _exame.circulacaocolateral = this.convertString(exame.circulacaocolateral);
+          _exame.circulacaocolateral = this.convertString(
+            exame.circulacaocolateral
+          );
           _exame.varizesofagicas = this.convertString(exame.varizesofagicas);
           _exame.hepatomegalia = this.convertString(exame.hepatomegalia);
           _exame.figadoendurecido = this.convertString(exame.figadoendurecido);
@@ -201,19 +210,21 @@ export class FormExameComponent extends BaseFormComponent implements OnInit {
           _exame.dataout = this.formatDate(_exame.dataout);
           _exame.datatransplante = this.formatDate(_exame.datatransplante);
 
-
           _exame.imagem.forEach((imagem: ImagemDTO) => {
-            imagem.src = `https://localhost:5001/v1/Imagem/${imagem.idimagem}/download`
+            imagem.src = `https://localhost:5001/v1/Imagem/${imagem.idimagem}/download`;
           });
 
           this.formulario.patchValue(_exame);
         });
-      } this.formulario.patchValue(Object.assign({}, JSON.parse(sessionStorage.getItem("exame") || "{}")));
+      }
+      this.formulario.patchValue(
+        Object.assign({}, JSON.parse(sessionStorage.getItem('exame') || '{}'))
+      );
     });
 
     this.formulario.valueChanges.subscribe(() => {
       this.setForm();
-    });    
+    });
   }
 
   openModal(template: TemplateRef<any>) {
@@ -225,18 +236,30 @@ export class FormExameComponent extends BaseFormComponent implements OnInit {
 
     this.click = true;
 
-    valueSubmit.transplantadofigado = this.convertNumber(valueSubmit.transplantadofigado);
-    valueSubmit.hipertensaoarterial = this.convertNumber(valueSubmit.hipertensaoarterial);
+    valueSubmit.transplantadofigado = this.convertNumber(
+      valueSubmit.transplantadofigado
+    );
+    valueSubmit.hipertensaoarterial = this.convertNumber(
+      valueSubmit.hipertensaoarterial
+    );
     valueSubmit.ictericia = this.convertNumber(valueSubmit.ictericia);
     valueSubmit.edema = this.convertNumber(valueSubmit.edema);
     valueSubmit.ascite = this.convertNumber(valueSubmit.ascite);
-    valueSubmit.circulacaocolateral = this.convertNumber(valueSubmit.circulacaocolateral);
-    valueSubmit.varizesofagicas = this.convertNumber(valueSubmit.varizesofagicas);
+    valueSubmit.circulacaocolateral = this.convertNumber(
+      valueSubmit.circulacaocolateral
+    );
+    valueSubmit.varizesofagicas = this.convertNumber(
+      valueSubmit.varizesofagicas
+    );
     valueSubmit.hepatomegalia = this.convertNumber(valueSubmit.hepatomegalia);
-    valueSubmit.figadoendurecido = this.convertNumber(valueSubmit.figadoendurecido);
+    valueSubmit.figadoendurecido = this.convertNumber(
+      valueSubmit.figadoendurecido
+    );
     valueSubmit.figadonodular = this.convertNumber(valueSubmit.figadonodular);
     valueSubmit.esplenomegalia = this.convertNumber(valueSubmit.esplenomegalia);
-    valueSubmit.doencaautoimune = this.convertNumber(valueSubmit.doencaautoimune);
+    valueSubmit.doencaautoimune = this.convertNumber(
+      valueSubmit.doencaautoimune
+    );
     valueSubmit.usamedicamento = this.convertNumber(valueSubmit.usamedicamento);
     valueSubmit.vhc = this.convertNumber(valueSubmit.vhc);
     valueSubmit.descvhc = this.convertNumber(valueSubmit.descvhc);
@@ -246,12 +269,10 @@ export class FormExameComponent extends BaseFormComponent implements OnInit {
     valueSubmit.deschiv = this.convertNumber(valueSubmit.deschiv);
 
     if (this.formulario.valid) {
-
       let msgSuccess = 'exame cadastrado com sucesso!';
       let msgError = 'Erro ao cadastrar exame, tente novamente!';
 
       if (this.formulario.value.idexame) {
-
         msgSuccess = 'exame atualizado com sucesso!';
         msgError = 'Erro ao atualizar exame, tente novamente!';
       }
@@ -265,7 +286,10 @@ export class FormExameComponent extends BaseFormComponent implements OnInit {
       );
     }
 
-    if (this.formulario.invalid) this.alertService.showAlertDanger('* Confira os campos em Vermelho ou com (*)');
+    if (this.formulario.invalid)
+      this.alertService.showAlertDanger(
+        '* Confira os campos em Vermelho ou com (*)'
+      );
   }
 
   incluirPaciente(paciente: PacienteDTO) {
@@ -355,14 +379,13 @@ export class FormExameComponent extends BaseFormComponent implements OnInit {
       exame.examemedicorespdiagnostico = [];
     }
 
-    exame.examemedicorespdiagnostico.push(
-      {
-        idexmeddiag: null,
-        idexame: exame.idexame,
-        idmedico: medico.idmedico,
+    exame.examemedicorespdiagnostico.push({
+      idexmeddiag: null,
+      idexame: exame.idexame,
+      idmedico: medico.idmedico,
 
-        medico: medico
-      });
+      medico: medico,
+    });
 
     this.formulario.patchValue(exame);
   }
@@ -387,14 +410,13 @@ export class FormExameComponent extends BaseFormComponent implements OnInit {
 
     if (exame.categoriaexame == null) exame.categoriaexame = [];
 
-    exame.categoriaexame.push(
-      {
-        idcategoriaexame: null,
-        idcategoria: categoria.idcategoria,
-        idexame: exame.idexame,
+    exame.categoriaexame.push({
+      idcategoriaexame: null,
+      idcategoria: categoria.idcategoria,
+      idexame: exame.idexame,
 
-        categoria: categoria
-      });
+      categoria: categoria,
+    });
 
     this.formulario.patchValue(exame);
   }
@@ -421,11 +443,11 @@ export class FormExameComponent extends BaseFormComponent implements OnInit {
       exame.imagem = [];
     }
 
-    this.imagemService.loadByID(idimagem).subscribe(imagem => {
+    this.imagemService.loadByID(idimagem).subscribe((imagem) => {
       exame.imagem.push(imagem);
     });
 
-    console.log("Imagens do form:", this.formulario.value.imagem);
+    console.log('Imagens do form:', this.formulario.value.imagem);
 
     this.setForm();
     this.formulario.patchValue(exame);
@@ -439,9 +461,8 @@ export class FormExameComponent extends BaseFormComponent implements OnInit {
     }
 
     exame.imagem.forEach((value, i) => {
-      if (value.nome == imagem.nome)
-        exame.imagem.splice(i, 1);
-    });   
+      if (value.nome == imagem.nome) exame.imagem.splice(i, 1);
+    });
 
     this.formulario.patchValue(exame);
   }
@@ -451,12 +472,15 @@ export class FormExameComponent extends BaseFormComponent implements OnInit {
   }
 
   setForm() {
-    sessionStorage.setItem("exame", JSON.stringify(Object.assign({}, this.formulario.value)));
+    sessionStorage.setItem(
+      'exame',
+      JSON.stringify(Object.assign({}, this.formulario.value))
+    );
   }
 
   onClear() {
     this.formulario.reset();
-    sessionStorage.removeItem("exame");
+    sessionStorage.removeItem('exame');
     this.click = false;
   }
 
@@ -465,13 +489,12 @@ export class FormExameComponent extends BaseFormComponent implements OnInit {
 
     if (exame.imagem == null) exame.imagem = [];
 
-    let msgSuccess = "Imagem enviada com sucesso!"
-    let msgError = "Erro ao enviar imagem!"
+    let msgSuccess = 'Imagem enviada com sucesso!';
+    let msgError = 'Erro ao enviar imagem!';
 
     this.imagemService.create(file).subscribe(
       (imagem) => {
-
-        imagem.src = `https://localhost:5001/v1/Imagem/${imagem.idimagem}/download`
+        imagem.src = `https://localhost:5001/v1/Imagem/${imagem.idimagem}/download`;
         exame.imagem.push(imagem);
         this.formulario.patchValue(exame);
         this.setForm();
@@ -499,4 +522,12 @@ export class FormExameComponent extends BaseFormComponent implements OnInit {
     return data;
   }
 
+  setNomeImagem(imagem: ImagemDTO, $event: any) {
+    imagem.nome = $event.srcElement.value;
+    this.setForm();
+  }
+
+  editarNomeImagem(idimagme: string) {
+    this.editarImagem = !this.editarImagem;
+  }
 }
